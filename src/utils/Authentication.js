0f5e75c0aaa2dd13745e3b561a870a1b47087fe1
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-12-13 13:37:56 +0600

    components and some pages added

---
+++ src/utils/Authentication.js
@@ -0,0 +1,26 @@
+// import jwtDecode from 'jwt-decode'
+
+export const isLoggedin = (requestedRole) => {
+    try {
+        const token = localStorage.getItem("token")
+        if (token) {
+            // let segments = token.split('.')
+            // atob(segments[0])
+            // let newtoken = jwtDecode(token)
+            // if (!newtoken || newtoken.exp * 1000 < Date.now() - (60 * 1000)) {
+            //     localStorage.removeItem("token")
+            //     return false
+            // }
+            // else {
+            //     return true
+            // }
+            return true
+
+        }
+        return false
+    } catch (err) {
+        localStorage.removeItem("token")
+        return false;
+    }
+
+};
